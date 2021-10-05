2b16c0340109e9eed4f8eaac58e9b05f5aa5c6a7
i850991 <arnaud.buchholz@sap.com>
2021-10-05 15:40:07 -0400

    useless

--- www/index.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const byId = id => document.getElementById(id)
-
-byId('login').addEventListener('click', async () => {
-  const response = await fetch('/url')
-  const json = await response.json()
-  window.open(json.url, '_blank', 'scrollbars=1,height=600,width=450,noopener,noreferrer')
-})
